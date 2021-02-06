3df827422377b6c7d1053b0cf9087ddb4ef1a7d7
SufyanIqbal1622 <bcsf16m16@gmai.com>
2021-02-06 23:32:07 +0500

    Order Controller done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,27 @@
 class OrdersController < ApplicationController
-  def index
-    @orders = Order.all
+ def show
+    @order = @current_order
+  end
+  def add_quantity
+  @order_item = OrderItem.find(params[:id])
+  @order_item.quantity += 1
+  @order_item.save
+  redirect_to order_path(@current_order)
+end
+
+def reduce_quantity
+  @order_item = OrderItem.find(params[:id])
+  if @order_item.quantity > 1
+    @order_item.quantity -= 1
+  end
+  @order_item.save
+  redirect_to order_path(@current_order)
+end
+
+  def destroy
+    @order = @current_order
+    @order.destroy
+    session[:order_id] = nil
+    redirect_to root_path
   end
 end
